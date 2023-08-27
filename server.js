2930d584734d2ff5618063a72b17ad6ee61e99f4
ALPHA25742 <160420737082@mjcollege.ac.in>
2023-08-27 15:31:29 +0530

    adding to git

---
+++ server.js
@@ -0,0 +1,24 @@
+const { instrument } = require("@socket.io/admin-ui");
+const io = require("socket.io")(5000, {
+  cors: {
+    origin: ["https://alpha25742.github.io/LegionCode"],
+  },
+});
+
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id; //coming from client
+  socket.join(id);
+
+  socket.on("send-message", ({ receipients, text }) => {
+    receipients.forEach((receipient) => {
+      const newReceipients = receipients.filter((r) => r !== receipient); //removing current receipient from receipient's list of receipients
+      newReceipients.push(id); //it is the id of sending the message
+      socket.broadcast.to(receipient).emit("receive-message", {
+        receipients: newReceipients,
+        sender: id,
+        text,
+      });
+    });
+  });
+});
+instrument(io, { auth: false });
